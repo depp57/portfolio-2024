080fb74e802a4815b13595dae2678d4ee24d9beb
Sacha Thommet <deppdu5758@gmail.com>
2024-03-17 13:30:12 +0100

    feat(i18n): add i18n translations (fr & en)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,41 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./src/lib/i18n.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["three"],
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      // Convert *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        use: ['@svgr/webpack'],
+      },
+    )
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
+
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      }
+    }
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
