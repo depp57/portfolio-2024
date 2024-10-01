e41e1bd804ba55fe87f6791cc24d82e5ac84ba7f
depp57 <deppdu5758@gmail.com>
2024-10-01 11:27:34 +0200

    feat: show a message to devs in the console

--- src/app/[lang]/Providers.tsx
+++ src/app/[lang]/Providers.tsx
@@ -2,7 +2,25 @@
 
 import { ThemeProvider } from 'next-themes';
 import { ReactNode } from 'react';
+import { usePathname } from 'next/navigation';
+import { useHomeStore } from '@/stores/homeStore';
 
-export function Providers({ children }: { children: ReactNode }) {
+function showBannerInConsole() {
+  console.log('%cHello there, developer!', 'color: #32ffce');
+  console.log(
+    '%cIf you’d like to get in touch, please feel free to reach out via the contact section.',
+    'color: #32ffce',
+  );
+  console.log('%c— Sacha', 'color: #777777');
+}
+
+export function Providers({ children }: Readonly<{ children: ReactNode }>) {
+  showBannerInConsole();
+
+  const pathname = usePathname();
+  if (pathname.split('/').length > 2) {
+    useHomeStore.setState({ isIntro: false });
+  }
+
   return <ThemeProvider disableTransitionOnChange>{children}</ThemeProvider>;
 }
