d41c9f37e64581ecc85bccf6fd435270ed459432
depp57 <deppdu5758@gmail.com>
2024-10-01 11:32:03 +0200

    feat(homepage): allow enable/disable of Open to Work button

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,8 @@ FROM base AS builder
 WORKDIR /app
 COPY --from=dependencies /app/node_modules ./node_modules
 COPY . .
-ENV NEXT_TELEMETRY_DISABLED 1
+ENV NEXT_TELEMETRY_DISABLED=1
+ENV NEXT_PUBLIC_IS_OPEN_TO_WORK=true
 RUN npm run build
 
 # Production image, copy all the files and run next
@@ -20,16 +21,15 @@ FROM base AS runner
 WORKDIR /app
 
 ENV NODE_ENV=production
-ENV NEXT_TELEMETRY_DISABLED 1
+ENV NEXT_TELEMETRY_DISABLED=1
 
-RUN addgroup --system --gid 1001 nodejs
-RUN adduser --system --uid 1001 nextjs
+RUN addgroup --system --gid 1001 nodejs \
+    && adduser --system --uid 1001 nextjs \
+    # Set the correct permission for prerender cache
+    && mkdir .next \
+    && chown nextjs:nodejs .next
 
 COPY --from=builder /app/public ./public
-
-# Set the correct permission for prerender cache
-RUN mkdir .next
-RUN chown nextjs:nodejs .next
 
 # Automatically leverage output traces to reduce image size
 # https://nextjs.org/docs/advanced-features/output-file-tracing
@@ -40,6 +40,6 @@ USER nextjs
 
 EXPOSE 3000
 
-ENV PORT 3000
+ENV PORT=3000
 
 CMD ["node", "server.js"]
--- src/components/pages/home/main/HomeHeader.tsx
+++ src/components/pages/home/main/HomeHeader.tsx
@@ -1,9 +1,10 @@
 import { Button } from '@/components/shared/button';
 import { EnvelopeClosedIcon } from '@radix-ui/react-icons';
-import Menu from '@/components/shared/menu/Menu';
 import { useTranslations } from 'next-intl';
 import { motion } from 'framer-motion';
+import { usePathname, useRouter } from 'next/navigation';
 import { cn } from '@/lib/utils';
+import Menu from '@/components/shared/menu/Menu';
 
 const variants = {
   initial: { x: 0, y: '-50%', opacity: 1 },
@@ -17,12 +18,18 @@ const variants2 = {
 
 export default function HomeHeader() {
   const t = useTranslations('home');
+  const router = useRouter();
+  const pathName = usePathname();
 
-  const MotionButton = motion(Button);
+  const isOpenToWork = process.env.NEXT_PUBLIC_IS_OPEN_TO_WORK === 'true';
+
+  const MotionButton = motion.create(Button);
 
   return (
     <header className="flex w-full justify-between items-center p-10">
       <MotionButton
+        type="button"
+        onClick={() => router.push(`${pathName}/about#contact-form`)}
         initial="initial"
         whileHover="hidden"
         size="lg"
@@ -37,10 +44,10 @@ export default function HomeHeader() {
         <EnvelopeClosedIcon className="mr-2 h-6 w-6 font-extrabold" />
         <div className="relative">
           <motion.span className="-translate-y-1/2 absolute" variants={variants} transition={{ duration: 0.3 }}>
-            {t('openToWork')}
+            {t(isOpenToWork ? 'openToWork' : 'closedToWork')}
           </motion.span>
           <motion.span className="absolute" variants={variants2} transition={{ duration: 0.3 }}>
-            {t('contactMe')}
+            {t(isOpenToWork ? 'contactMe' : 'contactMeAnyway')}
           </motion.span>
         </div>
       </MotionButton>
