112222770a366644e49f45dcc6cf690efb3f905c
Sacha Thommet <deppdu5758@gmail.com>
2024-10-09 14:59:56 +0200

    fix(home): fix HomeEntry animation

--- src/components/pages/home/intro/Intro.tsx
+++ src/components/pages/home/intro/Intro.tsx
@@ -15,24 +15,18 @@ export default function Intro() {
       <span className="h-32" />
 
       <div className="self-center">
-        <AnimatePresence>
+        <AnimatePresence mode="wait">
           {isExperienceLoaded ? (
             <motion.div
-              initial={{ position: 'absolute' }}
-              animate={{ position: 'unset' }}
-              transition={{ delay: 0.65, duration: 0 }}
+              key="soundSettings"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
             >
-              <motion.div
-                key="soundSettings"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.7, duration: 0.6 }}
-              >
-                <HomeEntry />
-              </motion.div>
+              <HomeEntry />
             </motion.div>
           ) : (
-            <motion.div key="loader" initial={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.6 }}>
+            <motion.div key="loader" initial={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
               <HomeLoader onLoaded={() => setIsExperienceLoaded(true)} />
             </motion.div>
           )}
