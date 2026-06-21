87d6313ac1a303c4da09369116cef5a2567c9510
Sacha Thommet <deppdu5758@gmail.com>
2026-06-21 17:55:21 +0200

    feat: no more preload project assets on the homepage

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,18 +1,9 @@
 import Home from '@/components/pages/home/Home';
-import ProjectPreloader from '@/components/pages/projects/ProjectPreloader';
 import { setRequestLocale } from 'next-intl/server';
-import { fetchProjects } from '@/lib/projects';
 
 export default async function Page({ params }: Readonly<{ params: Promise<{ lang: string }> }>) {
   const { lang } = await params;
   setRequestLocale(lang);
 
-  const projects = await fetchProjects(lang);
-
-  return (
-    <>
-      <Home />
-      <ProjectPreloader projects={projects} />
-    </>
-  );
+  return <Home />;
 }
